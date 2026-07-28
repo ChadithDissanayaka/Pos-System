6a62884965d26af99de29354c2182cd725a412e9
Chadith Dissanayaka <chadithdissanayaka@gmail.com>
2026-07-28 22:07:43 +0530

    feat: add CustomerRequestDTO with validation constraints for customer details

--- src/main/java/com/devstack/POS/dto/request/CustomerRequestDTO.java
+++ src/main/java/com/devstack/POS/dto/request/CustomerRequestDTO.java
@@ -21,6 +21,6 @@ public class CustomerRequestDTO {
 
     @Positive(message = "Salary must be a positive value")
     @DecimalMin(value = "0.01", message = "Salary must be at least 0.01")
-    @DecimalMin(value = "9999999.99", message = "Salary must be at least 9,999,999.99")
+    @DecimalMax(value = "9999999.99", message = "Salary must be at most 9,999,999.99")
     private double salary;
 }
